f74fe5ffcf513d5d85ea541c2ff56ca4efb1ad77
Alessandro Perrucci <ale.perrucci@gmail.com>
2017-10-12 19:24:00 +0200

    Can play artist top tracks from search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,74 +1,99 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {search} from '../modules/search';
-import TextField from 'material-ui/TextField';
-import { withRouter } from 'react-router';
-import Artist from './common/Artist';
-import Album from './common/Album';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { search } from "../modules/search";
+import {
+  loadAndPlayTopTracks,
+  loadAndPlayAlbumTracks
+} from "../modules/player";
+import TextField from "material-ui/TextField";
+import { withRouter } from "react-router";
+import Artist from "./common/Artist";
+import Album from "./common/Album";
 
-import PlayIcon from 'material-ui/svg-icons/av/play-circle-outline';
-import PauseIcon from 'material-ui/svg-icons/av/pause-circle-outline';
-
-
+import PlayIcon from "material-ui/svg-icons/av/play-circle-outline";
+import PauseIcon from "material-ui/svg-icons/av/pause-circle-outline";
 
 class Search extends Component {
   componentWillMount() {
     this.search(this.props.text);
   }
 
-  handleChange = (event) => {
-    this.props.history.push('/search/' + event.target.value)
+  handleChange = event => {
+    this.props.history.push("/search/" + event.target.value);
   };
 
   search = text => {
-    if (text !== undefined && text !== '')
-      this.props.search(text);
-  }
+    if (text !== undefined && text !== "") this.props.search(text);
+  };
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.text !== nextProps.text)
-      this.search(nextProps.text);
+    if (this.props.text !== nextProps.text) this.search(nextProps.text);
   }
+
+  playArtistTracks = artist => {
+    this.props.loadAndPlayTopTracks(artist.id);
+  };
+
+  playAlbumTracks = album => {
+    this.props.loadAndPlayAlbumTracks(album.id);
+  };
 
   render() {
     return (
       <div>
-        
-          <h2>Search</h2>
-          
-          <TextField
-              hintText="Type an artist"
-              floatingLabelText="Search" 
-              value={this.props.text}
-              onChange={this.handleChange}
-              style={{marginBottom:'2em'}}/>
+        <h2>Search</h2>
 
-          <h3>Artists</h3>
-          <div className="container">
-            {this.props.artists &&
-                <div className="row">
-                  {this.props.artists.map(a => <Artist key={a.id} artist={a}/>)}
-                </div>
-            }
+        <TextField
+          hintText="Type an artist"
+          floatingLabelText="Search"
+          value={this.props.text}
+          onChange={this.handleChange}
+          style={{ marginBottom: "2em" }}
+        />
+
+        <h3>Artists</h3>
+        <div className="container">
+          {this.props.artists && (
+            <div className="row">
+              {this.props.artists.map(a => (
+                <Artist
+                  key={a.id}
+                  artist={a}
+                  play={() => this.playArtistTracks(a)}
+                />
+              ))}
+            </div>
+          )}
 
           <h3>Albums</h3>
-            {this.props.albums &&
-                <div className="row">
-                  {this.props.albums.map(a => <Album key={a.id} album={a}/>)}
-                </div>
-            }
-          </div>
+          {this.props.albums && (
+            <div className="row">
+              {this.props.albums.map(a => (
+                <Album
+                  key={a.id}
+                  album={a}
+                  play={() => this.playAlbumTracks(a)}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     );
-  }s
+  }
+  s;
 }
 
 function mapStateToProps(state, ownProps) {
   return {
     artists: state.search.artists,
     albums: state.search.albums,
-    text: ownProps.match.params.text || ''
+    text: ownProps.match.params.text || ""
   };
 }
 
-export default connect(mapStateToProps, {search})(withRouter(Search));
+export default connect(mapStateToProps, {
+  search,
+  loadAndPlayTopTracks,
+  loadAndPlayAlbumTracks
+})(withRouter(Search));
--- src/modules/player.js
+++ src/modules/player.js
@@ -1,62 +1,130 @@
 //Actions
 
-const PLAY = 'player/PLAY';
-const PREVIOUS_TRACK = 'player/PREVIOUS_TRACK';
-const NEXT_TRACK = 'player/NEXT_TRACK';
-const ADD_TRACKS_QUEUE = 'player/ADD_TRACKS_QUEUE';
-const ADD_TRACKS_AND_PLAY = 'player/ADD_TRACKS_AND_PLAY';
-const REMOVE_TRACK_QUEUE = 'player/REMOVE_TRACK_QUEUE';
-const EMPTY_QUEUE = 'player/EMPTY_QUEUE';
-
+const PLAY = "player/PLAY";
+const PREVIOUS_TRACK = "player/PREVIOUS_TRACK";
+const NEXT_TRACK = "player/NEXT_TRACK";
+const ADD_TRACKS_QUEUE = "player/ADD_TRACKS_QUEUE";
+const ADD_TRACKS_AND_PLAY = "player/ADD_TRACKS_AND_PLAY";
+const REMOVE_TRACK_QUEUE = "player/REMOVE_TRACK_QUEUE";
+const EMPTY_QUEUE = "player/EMPTY_QUEUE";
+const LOAD_TOP_TRACKS_REQUEST = "player/LOAD_TOP_TRACKS_REQUEST";
+const LOAD_TOP_TRACKS_FAILED = "player/LOAD_TOP_TRACKS_FAILED";
 
 //Reducer
 
-const initialState = {queue:[], currentTrack:0};
+const initialState = { queue: [], currentTrack: 0 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case PLAY:
-      const index1 = state.queue.findIndex(x => x.id === action.payload)
-      return {...state, currentTrack: index1};
+      const index1 = state.queue.findIndex(x => x.id === action.payload);
+      return { ...state, currentTrack: index1 };
     case PREVIOUS_TRACK:
-      return {...state, currentTrack:state.currentTrack > 0 ? state.currentTrack - 1 : state.currentTrack};
+      return {
+        ...state,
+        currentTrack:
+          state.currentTrack > 0 ? state.currentTrack - 1 : state.currentTrack
+      };
     case NEXT_TRACK:
-      return {...state, currentTrack:state.currentTrack < state.queue.length - 1 ? state.currentTrack + 1 : state.currentTrack};
-      case ADD_TRACKS_QUEUE:
-      return {...state, queue:[...state.queue, ...action.payload]};
+      return {
+        ...state,
+        currentTrack:
+          state.currentTrack < state.queue.length - 1
+            ? state.currentTrack + 1
+            : state.currentTrack
+      };
+    case ADD_TRACKS_QUEUE:
+      return { ...state, queue: [...state.queue, ...action.payload] };
     case ADD_TRACKS_AND_PLAY:
-    debugger;
-      const index2 = action.payload.tracks.findIndex(x => x.id === action.payload.id)
-      return {...state, queue:[...action.payload.tracks], currentTrack:index2};
+      const index2 = action.payload.tracks.findIndex(
+        x => x.id === action.payload.id
+      );
+      return {
+        ...state,
+        queue: [...action.payload.tracks],
+        currentTrack: index2 >= 0 ? index2 : 0
+      };
     case EMPTY_QUEUE:
       return initialState;
   }
   return state;
 }
 
-
 //Action creators
 
 export function play(id) {
-  return { type: PLAY }
+  return { type: PLAY };
 }
 
 export function prevTrack() {
-  return { type: PREVIOUS_TRACK }
+  return { type: PREVIOUS_TRACK };
 }
 
 export function nextTrack() {
-  return { type: NEXT_TRACK }
+  return { type: NEXT_TRACK };
 }
 
 export function addTracksQueue(tracks) {
-  return { type: ADD_TRACKS_QUEUE, payload: tracks }
+  return { type: ADD_TRACKS_QUEUE, payload: tracks };
 }
 
 export function addTracksAndPlay(tracks, id) {
-  return { type: ADD_TRACKS_AND_PLAY, payload: {tracks,id} }
+  return { type: ADD_TRACKS_AND_PLAY, payload: { tracks, id } };
 }
 
 export function emptyQueue() {
-  return { type: EMPTY_QUEUE }
+  return { type: EMPTY_QUEUE };
+}
+
+export function loadTopTracksRequest(id) {
+  return { type: LOAD_TOP_TRACKS_REQUEST, payload: id };
+}
+
+export function loadTopTracksFailed(error) {
+  return { type: LOAD_TOP_TRACKS_FAILED, payload: error };
+}
+
+export function loadAndPlayTopTracks(artistId) {
+  return function(dispatch) {
+    dispatch(loadTopTracksRequest(artistId));
+    fetch(
+      `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=IT`,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("access_token")
+        }
+      }
+    )
+      .then(response => {
+        if (response.status === 200) {
+          response
+            .json()
+            .then(tracks => dispatch(addTracksAndPlay(tracks.tracks, null)));
+        } else dispatch(loadTopTracksFailed(response.message));
+      })
+      .catch(error => {
+        dispatch(loadTopTracksFailed(error));
+      });
+  };
+}
+
+export function loadAndPlayAlbumTracks(albumId) {
+  return function(dispatch) {
+    dispatch(loadTopTracksRequest(albumId));
+    fetch(`https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("access_token")
+      }
+    })
+      .then(response => {
+        if (response.status === 200) {
+          response
+            .json()
+            .then(tracks => dispatch(addTracksAndPlay(tracks.items, null)));
+        } else dispatch(loadTopTracksFailed(response.message));
+      })
+      .catch(error => {
+        dispatch(loadTopTracksFailed(error));
+      });
+  };
 }
